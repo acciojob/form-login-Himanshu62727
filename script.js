7a722271d6f4e8d61962493a8c27934c55a4967b
Himanshu Singh <124270542+Himanshu62727@users.noreply.github.com>
2024-09-14 19:06:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 function getFormvalue() {
     //Write your code here
-	alert("Rishabh Pant")
+	let s1 = document.querySelectorAll("input")
+	let firstName = s1[0].value
+	let lastName = s1[1].value
+	let fullName = firstName + " " + lastName
+	alert(fullName)
 }
